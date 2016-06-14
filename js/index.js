b620a20a1cfcd87b1f8909a46c48bcaaedac9d26
Kaushal Patel <kaushalpatel922@gmail.com>
2016-06-13 22:12:57 -0400

    working with bugs

--- js/index.js
+++ js/index.js
@@ -1,41 +1,82 @@
-console.log('loaded...');
 
-// Declare variables
-var current_number = Math.floor(Math.random() * 300);
-var count = 10;
-var incorrect = 0;
-var clock;
-var current_time;
+(function($){
+    $.fn.disableSelection = function() {
+        return this
+                 .attr('unselectable', 'on')
+                 .css('user-select', 'none')
+                 .on('selectstart', false);
+    };
+})(jQuery);
 
-//Define a function that checks for the possibilities
-function checkCurrentNumber() {
-  // 15 is first because if it's divisible by 15, it should return 'fizzbuzz' instead of running any other possibilities.
-  if (current_number % 15 == 0) {
-    return 'fizzbuzz';
-  } else if (current_number % 3 == 0) {
-    return 'fizz';
-  } else if (current_number % 5 == 0) {
-    return 'buzz';
-    // check for all of the above conditions, if none - print the current_number
-  } else {
-    return current_number;
+$(document).ready(function() {
+  // Declare variables
+  var current_number = Math.floor(Math.random() * 300);
+  var count = 10;
+  var incorrect = 0;
+  var clock;
+  var current_time;
+
+  //Define a function that checks for the possibilities
+  function checkCurrentNumber() {
+    // 15 is first because if it's divisible by 15, it should return 'fizzbuzz' instead of running any other possibilities.
+    if (current_number % 15 == 0) {
+      return 'fizzbuzz';
+    } else if (current_number % 3 == 0) {
+      return 'fizz';
+    } else if (current_number % 5 == 0) {
+      return 'buzz';
+      // check for all of the above conditions, if none - print the current_number
+    } else {
+      return current_number;
+    }
   }
-}
 
-// Updating status for Current number, wrong answers and counts left.
-function updateStats() {
-  $('.current_number').text(current_number);
-  $('.stat-incorrect').text("Incorrect: " + incorrect);
-  $('.stat-count').text("Turns left: " + count);
-}
+  // Updating status for Current number, wrong answers and counts left.
+  function updateStats() {
+    $('.current_number').text(current_number);
+    $('.stat-incorrect').text("Incorrect: " + incorrect);
+    $('.stat-count').text("Turns left: " + count);
+  }
 
-// At the end, update the result to show the data to the user.
-function updateEndCard() {
-  $('h3:nth-child(2)').text("Incorrect: " + incorrect);
-  $('h3:nth-child(3)').text("Accuracy: " + (10-miss)));
-}
+  // At the end, update the result to show the data to the user.
+  function updateEndCard() {
+    $('h3:nth-child(2)').text("Accuracy: " + (10-incorrect));
+    $('h3:nth-child(3)').text("Incorrect: " + incorrect);
+  }
 
-//When a user clicks the start button on the form execute this.
-$('.start_form').on('submit', function(e) {
+  //When a user clicks the start button on the form execute this.
+  $('.start_form').on('submit', function(e) {
+    e.preventDefault();
+    // hide the main display page
+    $('.title_card').hide();
+    //display the next page with input field.
+    $('.text').focus();
+    updateStats();
+  })
+
+  // When a user submits the answer on the input field
+  $('.input_form').on('submit', function(e) {
+    e.preventDefault();
+    var input = $('.text').val().toLowerCase().trim(),
+        test = checkCurrentNumber();
+
+    $('.text').val('');
+
+    if (test == input) {
+      count -- ;
+      if(count == 0) {
+        updateEndCard();
+        $('.end').show();
+      } else {
+        updateStats();
+      }
+    } else {
+        incorrect++;
+        updateStats();
+    }
+  });
+
+
+  $('.body').disableSelection();
 
 })
